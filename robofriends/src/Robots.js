b997710fbdf4118a8f4d27d0386cc4d9866e1024
Graham <graham.hervey@gmail.com>
2018-02-26 14:42:24 -0500

    Working react app

---
+++ robofriends/src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanna Graham',
+        username: 'Bret',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Steve Jacob',
+        username: 'Golden',
+        email: 'great@april.biz'
+    },
+    {
+        id: 3,
+        name: 'Sally Mae',
+        username: 'SM Supa',
+        email: 'thanks@april.biz'
+    },
+    {
+        id: 4,
+        name: 'Patrick Star',
+        username: 'Starman',
+        email: 'Rock@april.biz'
+    },
+    {
+        id: 5,
+        name: 'Killa Croc',
+        username: 'Villian',
+        email: 'Food@april.biz'
+    },
+    {
+        id: 6,
+        name: 'Bruce Wayne',
+        username: 'Batman',
+        email: 'Secret@april.biz'
+    },
+    {
+        id: 7,
+        name: 'Barry Allan',
+        username: 'Flash',
+        email: 'fastest@april.biz'
+    },
+    {
+        id: 8,
+        name: 'Clark Kent',
+        username: 'SuperMan',
+        email: 'Supes@april.biz'
+    },
+    {
+        id: 9,
+        name: 'Black Adam',
+        username: 'Egypt',
+        email: 'Egypt@april.biz'
+    },
+    {
+        id: 10,
+        name: 'Gal Gadot',
+        username: 'Wonder Woman',
+        email: 'Wonder@april.biz'
+    },
+]
